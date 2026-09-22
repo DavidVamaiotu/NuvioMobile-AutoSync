e8d78226c486e349c8649486539ab529843792dd
DavidVamaiotu <159152663+DavidVamaiotu@users.noreply.github.com>
2026-09-22 17:59:25 +0300

    Wire AutoSync retry into subtitle style panel

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/player/SubtitleModal.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/player/SubtitleModal.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.nuvio.app.core.ui.nuvio
 import com.nuvio.app.core.ui.withDuplicateSafeLazyKeys
+import com.nuvio.app.features.autosync.AutoSyncPlayerController
 import nuvio.composeapp.generated.resources.Res
 import nuvio.composeapp.generated.resources.addon_title
 import nuvio.composeapp.generated.resources.compose_player_built_in
@@ -74,6 +75,7 @@ fun SubtitleModal(
     subtitleDelayMs: Int,
     selectedAddonSubtitle: AddonSubtitle?,
     subtitleAutoSyncState: SubtitleAutoSyncUiState,
+    autoSyncController: AutoSyncPlayerController? = null,
     onBuiltInTrackSelected: (Int) -> Unit,
     onAddonSubtitleSelected: (AddonSubtitle) -> Unit,
     onFetchAddonSubtitles: () -> Unit,
@@ -304,6 +306,7 @@ fun SubtitleModal(
                                     subtitleDelayMs = subtitleDelayMs,
                                     selectedAddonSubtitle = effectiveSelectedAddonSubtitle,
                                     subtitleAutoSyncState = subtitleAutoSyncState,
+                                    autoSyncController = autoSyncController,
                                     isCompact = railMaxHeight < 420.dp,
                                     showHeader = false,
                                     onStyleChanged = onStyleChanged,
